42016c0ed4d5caa7734c0770a9db0cff3a760d3f
wdburke <williamburke@williamdburke.com>
2015-02-25 14:45:34 -0600

    Initial Code Load

---
+++ WDBWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WDBWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WDBWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WDBWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your friendly Austin based IT Consultant";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A brief history of...";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Feel free to reach out";
+
+            return View();
+        }
+    }
+}
